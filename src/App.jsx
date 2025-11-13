ed48c2c2654ecd4de5631d7b2078eeb4cfc590c5
ftmhabt <ftmh.a78@gmail.com>
2025-11-13 22:36:41 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PlanetScene from "./components/PlanetScene";
+
+function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <PlanetScene />
+    </div>
+  );
+}
+
+export default App;
